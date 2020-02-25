0cd4f5252da8ec2ecd1299d81b658d7f380ae826
zeucapua <zeucapua@gmail.com>
2020-02-25 14:58:17 -0800

    COMMIT [2/25/2020, by Zeu Capua] MainPage.dart  - added Map placeholder in a Stack with the existing Vehicle ListView, which now is overflowing and anchored at the bottom of the page  - able to press any item from the Vehicle ListView to navigate to a corresponding VehiclePage
     VehiclePage.dart
      - a page to display a chosen Vehicle's statistics like name, current and max capacity

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:mobility_appa/models/Vehicle.dart';
 import 'package:mobility_appa/models/VehicleDisplay.dart';
+import 'package:mobility_appa/pages/VehiclePage.dart';
 
 class MainPage extends StatefulWidget { @override MainPageState createState() => MainPageState(); }
 
@@ -11,21 +14,44 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Mobility Demo')),
       body: Container(
         child: SafeArea(
-          child: Center(
-            child: Container(
+          child: Stack(
+            //fit: StackFit.expand,
+            alignment: AlignmentDirectional.bottomCenter,
+            children: <Widget>[
 
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: vehicles.length,
-                itemBuilder: (context, index) {
-                  return VehicleDisplay(vehicle: vehicles[index],);
-                },
-              ),
-            ),
+              // to allow children to align at the very bottom center
+              Container(),
 
-          ),
+              // maps (currently just a screenshot as placeholder)
+              Image.asset('assets/images/map_placeholder.png'),
+
+              // vehicle route list
+              Positioned(
+                child: Container(
+                  height: 300.0,
+                  color: Colors.black26,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: vehicles.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => VehiclePage(vehicle: vehicles[index],))
+                        ),
+                        child: VehicleDisplay(vehicle: vehicles[index],),
+                      );
+                    },
+                  ),
+                ),
+              )
+
+            ],
+            overflow: Overflow.visible,
+          )
         ),
       ),
     );
@@ -36,7 +62,7 @@ class MainPageState extends State<MainPage> {
     super.initState();
 
     vehicles = [
-      Vehicle("258", 10, 40), Vehicle("71", 5, 40),
+      Vehicle("258", 10, 40), Vehicle("71", 5, 40), Vehicle("71", 5, 40), Vehicle("71", 5, 40),
     ];
 
 
---
+++ lib/pages/VehiclePage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:mobility_appa/models/Vehicle.dart';
+
+class VehiclePage extends StatefulWidget {
+  // state
+  VehiclePageState state;
+
+  // constructor
+  VehiclePage({Vehicle vehicle, Key key}) : super(key: key) { state = VehiclePageState(vehicle); }
+  @override VehiclePageState createState() => state;
+}
+
+class VehiclePageState extends State<VehiclePage> {
+  // instance variables
+  Vehicle vehicle;
+
+  // constructor
+  VehiclePageState(this.vehicle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                Text(vehicle.name, style: TextStyle(fontSize: 50),),
+
+                Row(
+                  children: <Widget>[ Icon(Icons.person_pin), Text(vehicle.currentCapacity.toString()) ],
+                ),
+
+                Row(
+                  children: <Widget>[ Icon(Icons.person), Text(vehicle.maxCapacity.toString()) ],
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.arrow_back), onPressed: () => Navigator.pop(context),),
+
+    );
+  }
+}
